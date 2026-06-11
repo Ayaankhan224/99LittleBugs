89cbc15959d251972230448bdf7b60f4dfbff303
Ayaan Khan <ayaankhan224271@gmail.com>
2026-06-11 18:39:21 +0530

    created navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { IoIosMenu } from "react-icons/io";
+import { IconContext } from "react-icons";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between">
+      <h1 className="font-[oran] text-4xl font-semibold leading-9">
+        99
+        <br />
+        Little
+        <br />
+        Bugs
+      </h1>
+      <div className="flex items-start gap-2">
+        <IconContext.Provider value={{ color:"white", size: "1.5em" }}>
+          <div className="bg-black rounded-full p-2 cursor-pointer hover:scale-110 duration-100 ease-in">
+            <IoIosMenu />
+          </div>
+        </IconContext.Provider>
+        <h3 className="text-white bg-[#E56E3A] py-2 px-5 rounded-full tracking-wider cursor-pointer hover:scale-110 duration-100 ease-in">SOURCE</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/Hero.jsx
+++ frontend/src/pages/Hero.jsx
@@ -1,22 +1,12 @@
-import React from "react";
-import Navbar from "../components/FirstPage/Navbar";
-import Title from "../components/FirstPage/Title";
+import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Hero = () => {
   return (
-    <div className="h-screen w-full"
-    style={{
-        background: `
-          radial-gradient(circle at 0% 55%, rgba(255,60,0,0.95) 0%, rgba(255,60,0,0.45) 22%, transparent 45%),
-          radial-gradient(circle at 100% 55%, rgba(255,0,255,0.75) 0%, rgba(180,0,255,0.45) 18%, transparent 40%),
-          radial-gradient(circle at 100% 35%, rgba(120,80,255,0.4) 0%, transparent 25%),
-          #020013
-        `,
-      }}>
-        <Navbar />
-        <Title />
+    <div className='m-6'>
+      <Navbar />
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
